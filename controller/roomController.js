df582a29f6f7481047b0c9a9e8129adb66b86200
opechapo <abolajiopeyemi000@gmail.com>
2024-11-28 16:25:03 +0100

    room controller finish

--- controller/roomController.js
+++ controller/roomController.js
@@ -77,4 +77,61 @@ const deleteRoom = asyncHandler(async (req, res) => {
 });
 
 
-module.exports = { createNewRoom,getAllRooms,getRoom,deleteRoom };
+const updateRoom = asyncHandler(async (req, res) => {
+  const roomId = req.params.roomId;
+  const { roomNumber, roomLocation, roomOccupancy, roomStatus, roomCapacity } =
+    req.body;
+
+  console.log({ roomOccupancy });
+
+  //   console.log({ originalArray });
+  try {
+    const room = await Room.findById(roomId);
+
+    if (!room) {
+      return res.status(404).json({ message: "Room not found" });
+    }
+
+    const currentRoomCapacity = room.roomCapacity;
+
+    const existingRoom = await Room.findOne({ roomNumber });
+    let originalRoomOccupancyArray;
+    if (existingRoom && room._id.toString() !== existingRoom._id.toString()) {
+      return res.status(400).json({ message: "Room already exist" });
+    }
+
+    if (roomOccupancy) {
+      originalRoomOccupancyArray = JSON.parse(roomOccupancy);
+
+      const isArray = Array.isArray(originalRoomOccupancyArray);
+      if (!isArray) {
+        return res
+          .status(400)
+          .json({ message: "Room occupancy should be an array" });
+      }
+
+      if (
+        (roomCapacity && originalRoomOccupancyArray.length > roomCapacity) ||
+        (!roomCapacity &&
+          originalRoomOccupancyArray.length > currentRoomCapacity)
+      ) {
+        return res.status(400).json({
+          message: "Room occupancy should be less than room capacity",
+        });
+      }
+    }
+
+    room.roomNumber = roomNumber || room.roomNumber;
+    room.roomCapacity = roomCapacity || room.roomCapacity;
+    room.roomLocation = roomLocation || room.roomLocation;
+    room.roomOccupancy = originalRoomOccupancyArray || room.roomOccupancy;
+    room.roomStatus = roomStatus || room.roomStatus;
+    await room.save();
+    res.status(200).json(room);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+module.exports = { createNewRoom,getAllRooms,getRoom,deleteRoom,updateRoom };
--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const { protectAdmin } = require('../middleware/authMiddleware');
 
 
-const {createNewRoom,getAllRooms,getRoom,deleteRoom} = require('../controller/roomController');
+const {createNewRoom,getAllRooms,getRoom,deleteRoom,updateRoom} = require('../controller/roomController');
 
 router.post('/create-room', createNewRoom);
 router.get('/',protectAdmin, getAllRooms);
 router.get('/:roomId', protectAdmin,getRoom );
+router.patch('/update-room/:roomId', protectAdmin,updateRoom );
 router.delete('/:roomId', protectAdmin, deleteRoom);
 
 
